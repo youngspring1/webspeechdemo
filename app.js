392c83b9a370eeb3b8ebf3970e4f03d1c2d14ebb
youngspring1 <youngspring1@163.com>
2016-09-06 14:09:20 +0800

    update index

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var content = document.getElementById('content');
 var speech = new webkitSpeechRecognition();
 
 //言語を日本語に設定
-speech.lang = "zh";
+//speech.lang = "en";
+speech.lang = "jp";
+//speech.lang = "zh";
+
 
 //ボタンクリックで認識開始
 btn.addEventListener('click', function() {
