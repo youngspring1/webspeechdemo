74c65241f55c37f92cd21c650e56debc838a6df1
youngspring1 <youngspring1@163.com>
2016-10-10 17:38:29 +0800

    update rate volume

--- app.js
+++ app.js
@@ -111,8 +111,8 @@ function say(text) {
   var voices = window.speechSynthesis.getVoices();
   msg.voice = voices[10]; // Note: some voices don't support altering params
   msg.voiceURI = 'native';
-  msg.volume = 1; // 0 to 1
-  msg.rate = 1; // 0.1 to 10
+  msg.volume = 0.5; // 0 to 1
+  msg.rate = 5; // 0.1 to 10
   msg.pitch = 2; //0 to 2
   msg.text = text;
   msg.lang = 'ja-JP';
